db030d6fba1c887fba69674fe09e82c916bb95bc
dacmarcell <dacmarcell@gmail.com>
2024-12-22 00:12:37 -0300

    Add DeployURL property to Project model

--- Models/Project.cs
+++ Models/Project.cs
@@ -6,6 +6,7 @@ namespace portfolio_api.Models
         public required string Name {get; set;} = string.Empty;
         public string Description {get;set;} = string.Empty;
         public string GithubURL {get;set;} = string.Empty;
+        public string DeployURL {get;set;} = string.Empty;
         public DateTime CreatedAt {get; set;} = DateTime.Now;
         public required int ProfileID {get;set;}
         public required Profile Profile {get;set;}
