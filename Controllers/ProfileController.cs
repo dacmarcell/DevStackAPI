07d9782c7d25eb295e882abebf2cc03bfeee2580
dacmarcell <dacmarcell@gmail.com>
2024-12-27 18:30:35 -0300

    feat: implement connect and disconnect functionality for social media accounts in profile

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using portfolio_api.Data;
 using portfolio_api.Dtos.Profile;
 using portfolio_api.Models;
@@ -15,40 +16,66 @@ namespace portfolio_api.Controllers{
         }
 
         [HttpGet(Name = "GetProfile")]
-        public IEnumerable<Profile> Get(){
-          return [.. _dbContext.Profiles];
+        public ActionResult<Profile> GetProfile(){
+            var profiles = _dbContext.Profiles;
+            return Ok(profiles);
         }
 
         [HttpGet("{id}", Name = "GetProfileById")]
-        public async Task<ActionResult<Profile>> Get(int id){
+        public async Task<ActionResult<Profile>> GetProfileById(int id){
             var profile = await _dbContext.Profiles.FindAsync(id);
-            
             if (profile is null){
-                return NotFound();
+                return NotFound("Profile not found");
             }
 
-            return profile;
+            return Ok(profile);
         }
 
         [HttpPost(Name = "CreateProfile")]
-        public async Task<ActionResult<Profile>> Post(Profile profile){
+        public async Task<ActionResult<Profile>> CreateProfile(Profile profile){
             _dbContext.Profiles.Add(profile);
             await _dbContext.SaveChangesAsync();
             return CreatedAtRoute("CreateProfile", new { id = profile.ID }, profile);
         }
 
+        [HttpPost("{id}/social-media", Name = "ConnectOrDisconnectSocialMedia")]
+        public async Task<ActionResult<Profile>> ConnectOrDisconnectSocialMedia(int id, [FromBody] ConnectOrDisconnectSocialMediaDto body){
+            var profile = await _dbContext.Profiles
+                .Include(profile => profile.SocialMedia)
+                .FirstOrDefaultAsync(profile => profile.ID == id);
+            if (profile is null) {
+                return NotFound("Profile not found");
+            }
+
+            var socialMedia = await _dbContext.SocialMedias.FindAsync(body.SocialMediaID);
+            if (socialMedia is null) {
+                return NotFound("Social Media not found");
+            }
+
+            if (body.Kind == SocialMediaKind.Connect) {
+                profile.SocialMedia.Add(socialMedia);
+            } else if (body.Kind == SocialMediaKind.Disconnect) {
+                profile.SocialMedia.Remove(socialMedia);
+            } else {
+                return BadRequest("Invalid Kind");
+            }
+            
+            _dbContext.Profiles.Update(profile);
+
+            await _dbContext.SaveChangesAsync();
+            return Ok(profile);
+        }
+
         [HttpPatch("{id}", Name = "UpdateProfile")]
-        public async Task<ActionResult<Profile>> Patch(int id, [FromBody] Profile body){
+        public async Task<ActionResult<Profile>> UpdateProfile(int id, [FromBody] Profile body){
             var foundProfile = await _dbContext.Profiles.FindAsync(id);
             if(foundProfile is null){
-                return NotFound();
+                return NotFound("Profile not found");
             }
 
             if(!string.IsNullOrWhiteSpace(body.Name)){
                 foundProfile.Name = body.Name;
-            }
-
-            if(!string.IsNullOrWhiteSpace(body.AboutMe)){
+            } else if (!string.IsNullOrWhiteSpace(body.AboutMe)){
                 foundProfile.AboutMe = body.AboutMe;
             }
 
@@ -59,42 +86,16 @@ namespace portfolio_api.Controllers{
         }
 
         [HttpDelete("{id}", Name = "DeleteProfile")]
-        public async Task<ActionResult<Profile>> Delete(int id){
+        public async Task<ActionResult<Profile>> DeleteProfile(int id){
             var profile = await _dbContext.Profiles.FindAsync(id);
             if(profile is null){
-                return NotFound();
+                return NotFound("Profile not found");
             }
 
             _dbContext.Profiles.Remove(profile);
             await _dbContext.SaveChangesAsync();
 
             return NoContent();
-        }
-
-        [HttpPost("{id}/social-media", Name = "ConnectOrDisconnectSocialMedia")]
-        public async Task<ActionResult<Profile>> ConnectSocialMedia(int id, [FromBody] ConnectOrDisconnectSocialMediaDto body){
-            var profile = await _dbContext.Profiles.FindAsync(id);
-            if(profile is null){
-                return NotFound();
-            }
-
-            var socialMedia = await _dbContext.SocialMedias.FindAsync(body.SocialMediaID);
-            if(socialMedia is null){
-                return NotFound();
-            }
-
-            if(body.Kind == SocialMediaKind.Connect){
-                profile.SocialMedia.Add(socialMedia);
-            } else if (body.Kind == SocialMediaKind.Disconnect) {
-                profile.SocialMedia.Remove(socialMedia);
-            } else {
-                return BadRequest();
-            }
-            
-            _dbContext.Profiles.Update(profile);
-
-            await _dbContext.SaveChangesAsync();
-            return Ok(profile);
         }
     }
 }
