077cb0a95d6adbb42005c77c25d4a4387217b8af
dacmarcell <dacmarcell@gmail.com>
2024-12-27 00:43:00 -0300

    Add ApplicationDBContext for Entity Framework Core database context

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using portfolio_api.Models;
+
+namespace portfolio_api.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions): base(dbContextOptions) 
+        {}
+
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+    }
+}
