2b96c8d68ac6ab815a5d8aa4239433101f96dc26
dacmarcell <dacmarcell@gmail.com>
2024-12-27 18:30:39 -0300

    feat: enhance Swagger configuration with versioning and conflict resolution

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using portfolio_api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
     ?? throw new InvalidOperationException("Connection string not found in configuration");
 
-builder.Services.AddEndpointsApiExplorer().AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer().AddSwaggerGen(services => {
+    services.SwaggerDoc("v1", new OpenApiInfo {Title = "PortfolioApi", Version = "v1"});
+    services.ResolveConflictingActions(options => options.First());
+});
+
 builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(connectionString));
 
