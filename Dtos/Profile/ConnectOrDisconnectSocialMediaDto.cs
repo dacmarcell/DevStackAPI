aa38903b3e758d0d3ee1638cfb778fa13fbbc13e
dacmarcell <dacmarcell@gmail.com>
2024-12-27 18:30:45 -0300

    refactor: update SocialMediaKind enum for clarity and serialization support

--- Dtos/Profile/ConnectOrDisconnectSocialMediaDto.cs
+++ Dtos/Profile/ConnectOrDisconnectSocialMediaDto.cs
@@ -1,7 +1,18 @@
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json.Converters;
+
 namespace portfolio_api.Dtos.Profile {
-    public enum SocialMediaKind {Connect, Disconnect};
+    // [JsonConverter(typeof(StringEnumConverter))]
+    public enum SocialMediaKind {
+        // [EnumMember(Value = "connect")]
+        Connect,
+        // [EnumMember(Value = "disconnect")]
+        Disconnect
+    };
     public class ConnectOrDisconnectSocialMediaDto {
         public required string SocialMediaID { get; set; }
+        // [JsonConverter(typeof(StringEnumConverter))]
         public required SocialMediaKind Kind { get; set; }
     }
 }
