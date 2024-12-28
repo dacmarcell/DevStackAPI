22b47b821fbe6407821d9884f6bafea0c03d3d3b
dacmarcell <dacmarcell@gmail.com>
2024-12-27 21:08:24 -0300

    feat: update UpdateSocialMediaDto to allow nullable properties and add Profile association

--- Dtos/UpdateSocialMediaDto.cs
+++ Dtos/UpdateSocialMediaDto.cs
@@ -1,12 +1,13 @@
 using portfolio_api.Enums;
+using portfolio_api.Models;
 
 namespace portfolio_api.Dtos
 {
     public class UpdateSocialMediaDto
     {
-        public int ID { get; set; }
-        public required SocialMediaNames Name { get; set; }
-        public required string URL { get; set; }
-        public required int ProfileID { get; set; }
+        public SocialMediaNames? Name { get; set; }
+        public  string? URL { get; set; }
+        public  int? ProfileID { get; set; }
+        public  Profile? Profile { get; set; }
     }
 }
