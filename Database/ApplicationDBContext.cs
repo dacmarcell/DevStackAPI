0306f67643426cc7f5ed71db09adeea6ba9b39cd
dacmarcell <dacmarcell@gmail.com>
2024-12-28 13:36:49 -0300

    fix: remove unnecessary blank line in ApplicationDBContext

--- Database/ApplicationDBContext.cs
+++ Database/ApplicationDBContext.cs
@@ -7,7 +7,6 @@ namespace portfolio_api.Data
     {
         public ApplicationDBContext(DbContextOptions dbContextOptions): base(dbContextOptions) 
         {}
-
         public DbSet<Profile> Profiles { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<Post> Posts { get; set; }
