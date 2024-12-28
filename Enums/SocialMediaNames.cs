bcbadbf832f7f4e410f86c38f826a305785aec7e
dacmarcell <dacmarcell@gmail.com>
2024-12-28 13:36:56 -0300

    fix: add JsonStringEnumConverter to SocialMediaNames enum for JSON serialization

--- Enums/SocialMediaNames.cs
+++ Enums/SocialMediaNames.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
+
 namespace portfolio_api.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum SocialMediaNames
     {
         Github,
