c4f7186e818be3f633e8d4a8a4855905e626c52c
dacmarcell <dacmarcell@gmail.com>
2024-12-28 15:52:15 -0300

    feat: add column type specification for SocialMediaNames in SocialMedia model

--- Models/SocialMedia.cs
+++ Models/SocialMedia.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using portfolio_api.Enums;
 
 namespace portfolio_api.Models {
     public class SocialMedia{
         public int ID {get; set;}
+        [Column(TypeName = "nvarchar(24)")]
         public required SocialMediaNames Name {get; set;}
         public required string URL {get; set;}
         public required int ProfileID {get; set;}
