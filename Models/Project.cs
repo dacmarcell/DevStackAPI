a17cf7ead57e418f7d8342e6f422315876779526
dacmarcell <dacmarcell@gmail.com>
2024-12-31 18:17:01 -0300

    style: format properties in Project model for consistency

--- Models/Project.cs
+++ Models/Project.cs
@@ -3,13 +3,13 @@ namespace portfolio_api.Models
     public class Project
     {
         public int ID { get; set; }
-        public required string Name {get; set;} = string.Empty;
-        public string Description {get;set;} = string.Empty;
-        public string GithubURL {get;set;} = string.Empty;
-        public string DeployURL {get;set;} = string.Empty;
-        public DateTime CreatedAt {get; set;} = DateTime.Now;
-        public required int ProfileID {get;set;}
-        public required Profile Profile {get;set;}
+        public required string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string GithubURL { get; set; } = string.Empty;
+        public string DeployURL { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public required int ProfileID { get; set; }
+        public required Profile Profile { get; set; }
 
     }
 }
